64f7f4c5a6de126424128770b8f203e118831f03
jdbhodgson@Giles <17334046+jdbhodgson@users.noreply.github.com>
2018-06-30 21:24:01 +0100

    added weierstrass params to config file, started pylint compliance of weierstrass.py

--- weierstrass.py
+++ weierstrass.py
@@ -4,28 +4,29 @@
 '''
 import random
 import numpy as np
+import configparser
 from scipy.interpolate import interp1d
 from scipy.interpolate import CubicSpline
 from scipy.interpolate import splev, splrep
 import matplotlib.pyplot as plt
 import pso
 
-def extend_odd(x):
+def extend_odd(y):
     '''Creates an odd extention of an array'''
-    x_odd = -np.flipud(x)
-    x_odd = np.append(x_odd, 0)
-    x = np.append(x_odd, x)
-    return x
+    y_odd = -np.flipud(y)
+    y_odd = np.append(y_odd, 0)
+    y = np.append(y_odd, y)
+    return y
 
-def extend_even(x):
+def extend_even(y):
     '''Creates an even extention of an array'''
-    x_zero = x[0]
-    x = np.delete(x, 0)
-    x_even = np.flipud(x)
-    x_even = np.append(x_even, x_zero)
-    x = np.append(x_even, x)
+    y_zero = y[0]
+    y = np.delete(y, 0)
+    y_even = np.flipud(y)
+    y_even = np.append(y_even, y_zero)
+    y = np.append(y_even, y)
 
-    return x
+    return y
 
 def fitness(points):
     '''
@@ -35,24 +36,24 @@ def fitness(points):
        implies better approximation
     '''
 
-    if type == 'odd':
+    if parity == 'odd':
         y_ext = extend_odd(points)
-    elif type == 'even':
+    elif parity == 'even':
         y_ext = extend_even(points)
     x_ext = extend_odd(x)
 
     tck = splrep(x_ext, y_ext)
 
-    G = np.empty(num_a) * 0
-    
+    G_func = np.empty(num_a) * 0
+
     for i in range(num_a):
-        xi = np.linspace(A[i]-cutoff, A[i]+cutoff, num=num_a)
-        Ker = np.exp(-(xi-A[i])**2/2.)/(np.sqrt(2.*np.pi))
-        G[i] = np.trapz(Ker*splev(xi, tck), xi)
+        x_a = np.linspace(A[i]-cutoff, A[i]+cutoff, num=num_a)
+        Kernel = np.exp(-(x_a-A[i])**2/2.)/(np.sqrt(2.*np.pi))
+        G_func[i] = np.trapz(Kernel*splev(x_a, tck), x_a)
 
-    Integral = np.trapz((G-Target)**2, A)
+    integral = np.trapz((G_func-Target)**2, A)
 
-    return Integral
+    return integral
 
 random.seed(12)
 
@@ -61,16 +62,21 @@ A0 = 20.
 cutoff = 4
 num_x = 4
 num_a = 20
+
+config = configparser.ConfigParser()
+config.read('params.cfg')
+params = [float(config['WEIERSTRASS'][key])
+          for key in config['WEIERSTRASS']]
 x = np.linspace(0, 1, num=num_x+1)**2 *(A0 + cutoff)
 x = x[1:]
-type = 'odd'
+parity = 'odd'
 
 A = np.linspace(-A0, A0, num=num_a)
 Target = np.tanh(A*0.45)
 
 x2 = np.linspace(-A0-cutoff, A0+cutoff, num=301)
 
-lower_bound, upper_bound = 0,2
+lower_bound, upper_bound = 0, 2
 bounds = [[lower_bound for i in range(num_x)],
           [upper_bound for i in range(num_x)]]
 swarm = pso.Swarm(20, num_x, fitness, bounds)
@@ -79,9 +85,9 @@ def plotData(pos):
 
     fig = plt.figure()
     y = pos
-    if type == 'odd':
+    if parity == 'odd':
         yExt = extend_odd(y)
-    elif type == 'even':
+    elif parity == 'even':
         yExt = extend_even(y)
 
     xExt = extend_odd(x)
