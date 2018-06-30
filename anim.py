a94b55f2df714c2f950a90bf1b4c9587fd60cb2c
jdbhodgson@Giles <17334046+jdbhodgson@users.noreply.github.com>
2018-06-30 15:31:33 +0100

    anim.py now 100% pylint compliant

--- anim.py
+++ anim.py
@@ -1,61 +1,77 @@
+'''
+ Custom class for animations.
+ Generalised from matplotlib docs example
+ https://matplotlib.org/gallery/animation/animate_decay.html
+'''
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-# Custom class for animations.
-# Generalised from matplotlib docs example
-# https://matplotlib.org/gallery/animation/animate_decay.html
 
 class Animator(object):
+    '''
+       A python class which holds an animator. The animator
+       is capable of updating a matplotlib plot in realtime
+       as a python program is running.
+       Generalised from matplotlib docs example
+       https://matplotlib.org/gallery/animation/animate_decay.html
+    '''
+    # pylint: disable=too-many-instance-attributes
 
     def __init__(self):
-        self.fig, self.ax = plt.subplots()
-        self.line, = self.ax.plot([], [], lw=2)
-        self.ax.grid()
+        self.fig, self.axis = plt.subplots()
+        self.line, = self.axis.plot([], [], lw=2)
+        self.axis.grid()
         self.xdata, self.ydata = [], []
-        self.xrange_0 = (0,10)
-        self.yrange_0 = (-1.1,1.1)
+        self.xrange_0 = (0, 10)
+        self.yrange_0 = (-1.1, 1.1)
         self.init_axes()
         self.yscale = 'linear'
-        
+        self.xmin, self.xmax = self.axis.get_xlim()
+        self.ymin, self.ymax = self.axis.get_ylim()
+
     def init_axes(self):
-        self.ax.set_ylim(self.yrange_0)
-        self.ax.set_xlim(self.xrange_0)
+        '''Initiates the plot axes'''
+
+        self.axis.set_ylim(self.yrange_0)
+        self.axis.set_xlim(self.xrange_0)
         del self.xdata[:]
         del self.ydata[:]
         self.line.set_data(self.xdata, self.ydata)
         return self.line,
-        
-    def data_gen(self,t=0):
-        cnt = 0
-        while cnt < 1000:
-            cnt += 1
-            t += 0.1
-            yield t, np.sin(2*np.pi*t) * np.exp(-t/10.)
 
     def run(self, data):
-        # update the data
+        '''updates the plot data'''
         t, y = data
         self.xdata.append(t)
         self.ydata.append(y)
-        self.xmin, self.xmax = self.ax.get_xlim()
-        self.ymin, self.ymax = self.ax.get_ylim()
+        self.xmin, self.xmax = self.axis.get_xlim()
+        self.ymin, self.ymax = self.axis.get_ylim()
 
         if t >= self.xmax:
-            self.ax.set_xlim(self.xmin, 1.2*self.xmax)
-            self.ax.figure.canvas.draw()
+            self.axis.set_xlim(self.xmin, 1.2*self.xmax)
+            self.axis.figure.canvas.draw()
         if min(self.ydata) <= self.ymin:
-            self.ax.set_ylim(self.ymin/10, self.ymax)
-            self.ax.figure.canvas.draw()
+            self.axis.set_ylim(self.ymin/10, self.ymax)
+            self.axis.figure.canvas.draw()
 
         self.line.set_data(self.xdata, self.ydata)
-        self.ax.set_yscale(self.yscale)
+        self.axis.set_yscale(self.yscale)
 
         return self.line,
 
     def animate(self):
-        ani = animation.FuncAnimation(self.fig,
-            self.run, self.data_gen, blit=False, interval=10,
-            repeat=False, init_func=self.init_axes)
+        '''Plots and shows the animation'''
+        animation.FuncAnimation(self.fig, self.run,
+                                data_gen, blit=False,
+                                interval=10, repeat=False,
+                                init_func=self.init_axes)
         plt.show()
-        
+
+def data_gen(t=0):
+    '''generates data for a test function'''
+    cnt = 0
+    while cnt < 1000:
+        cnt += 1
+        t += 0.1
+        yield t, np.sin(2*np.pi*t) * np.exp(-t/10.)
